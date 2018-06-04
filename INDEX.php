4b8ce824482418be3c9eeac54035f9388005389a
El Kandoussi Adnan <alzi0masta@gmail.com>
2018-06-04 14:51:57 +0200

    Le premier commit

---
+++ INDEX.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+   <head>
+
+    	<meta charset="UTF-8">
+    	<title>Adn's World</title>
+    	<link href="monstyle.css" rel="stylesheet" media="all">
+   </head>
+
+  <body>
+
+    <header>
+     <!-- Afficher les boutons S'inscrire et Se connecter aucune session n'est active--> 
+    <?php
+    session_start();
+    if(!isset($_SESSION['id']) AND !isset($_SESSION['name']))
+    {
+    ?>
+      <div class="headlog"><p><a href="REGISTER.php">S'inscrire</a><br/>
+      <a href="LOGIN.php">Se connecter</a></p></div>
+    <?php
+    }
+
+    ?>
+    </header>
+    <!-- Afficher le bouton Se déconnecter si un utilisateur est connecté-->
+    <?php
+    
+    if(isset($_SESSION['id']) AND isset($_SESSION['name']))
+    {
+    ?>
+      <div class="disconnect"><p><a href="DISCONNECT.php">Se déconnecter</a></p></div>
+    <?php
+    }
+
+    ?>
+
+		
+    <br/><br/><br/><br/>
+
+    <h1>Bienvenue sur MON espace membre  </h1>
+    <br/><br/><br/>
+    
+    <p><a href="PUBLI.php">Espace de publication</a></p>
+   
+	 <br><br><br>
+
+	 <p>
+	 <strong>Evénements :</strong>
+	 
+	 </p>
+	 <br>
+	 <p>
+	 <a href="EVENTSC2.php">Tournoi StarCraft 2</a> 
+	 </p>
+   <br>
+	 <p>
+	 <a href="EVENTWC3.php">LAN Warcraft 3</a>	
+	 </p>
+	
+  </body>
+</html>
